a13ec54071b4efd3a5671e72e558fdba9cbba25f
Haris Milonas <haris-mil_97@hotmail.com>
2022-10-03 09:22:08 +0300

    Adding a articles listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,8 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])    # @ ειναι instance variable  για να μπορω να την χρησιμοποιησω και στο view moy 
     end                                         # to params[:id] einai ena hash kai to pairnw apo to URL: localhost3000/articles/1    opou 1 to id 
-
-
+    def index
+        @articles = Article.all 
+    end
 
 end
