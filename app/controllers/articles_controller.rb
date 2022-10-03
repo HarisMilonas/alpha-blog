7ea76be90a566fcd32319f0ee99e507cc5c1f342
Haris Milonas <haris-mil_97@hotmail.com>
2022-10-03 11:31:28 +0300

    Adding Edit and Update actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,12 +7,30 @@ class ArticlesController < ApplicationController
         @articles = Article.all 
     end
     def new 
-
+        @article = Article.new
     end
 
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
-        @article.save
+       if  @article.save 
+            flash[:notice] = "Article was created successfully"
         redirect_to @article   #kanonika einai redirect_to article_path(@article) alla xrisimopoeitai toso suxna poy h ruby exei suntomografia gia auto
+       else
+        render 'new'
+       end
+    end
+
+    def edit 
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])        #apo to URL pare to id pou briskomaste twra 
+      if   @article.update(params.require(:article).permit(:title, :description))    # kane article update () me to params.require pairnw pali to articles apo to URL kai kanw permit ta 2 auta pedia na ginoun updated 
+            flash[:notice] = "Article was updated successfully"             # bgazw ena mhnuma an ola pane kala oti egine epityxhs ananewsh 
+            redirect_to @article    
+      else
+            render 'edit'                              # an oxi render pali thn edit selida(ksanafortwse thn)
+     end
     end
 end
