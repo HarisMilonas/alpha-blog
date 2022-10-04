15066df9f00d8cc214e17c9ddf18fcbb36f07206
Haris Milonas <haris-mil_97@hotmail.com>
2022-10-04 11:23:01 +0300

    DRYing code using render and private methods, also adding paths to all pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,9 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show,:edit,:update,:destroy]      #gia na mporei na diabasei prwta thn methodo kai meta na paei ston kwdika
 
     def show
-        @article = Article.find(params[:id])    # @ ειναι instance variable  για να μπορω να την χρησιμοποιησω και στο view moy 
-    end                                         # to params[:id] einai ena hash kai to pairnw apo to URL: localhost3000/articles/1    opou 1 to id 
+           
+    end                                        
     def index
         @articles = Article.all 
     end
@@ -11,7 +12,7 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
        if  @article.save 
             flash[:notice] = "Article was created successfully"
         redirect_to @article   #kanonika einai redirect_to article_path(@article) alla xrisimopoeitai toso suxna poy h ruby exei suntomografia gia auto
@@ -20,13 +21,11 @@ class ArticlesController < ApplicationController
        end
     end
 
-    def edit 
-        @article = Article.find(params[:id])
+    def edit  
     end
 
     def update
-        @article = Article.find(params[:id])        #apo to URL pare to id pou briskomaste twra 
-      if   @article.update(params.require(:article).permit(:title, :description))    # kane article update () me to params.require pairnw pali to articles apo to URL kai kanw permit ta 2 auta pedia na ginoun updated 
+      if   @article.update(article_params)    # kane article update () me to params.require pairnw pali to articles apo to URL kai kanw permit ta 2 auta pedia na ginoun updated 
             flash[:notice] = "Article was updated successfully"             # bgazw ena mhnuma an ola pane kala oti egine epityxhs ananewsh 
             redirect_to @article    
       else
@@ -35,8 +34,22 @@ class ArticlesController < ApplicationController
     end
     
     def destroy
-        @article = Article.find(params[:id]) 
         @article.destroy
         redirect_to articles_path
     end
+
+
+    private 
+
+
+    def set_article
+     @article = Article.find(params[:id])  # @ ειναι instance variable  για να μπορω να την χρησιμοποιησω και στο view moy
+    end                                      # to params[:id] einai ena hash kai to pairnw apo to URL: localhost3000/articles/1    opou 1 to id 
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+
 end
+
+
