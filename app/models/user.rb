fb35372971b52304e320cc33ebb60309366d5745
Haris Milonas <haris-mil_97@hotmail.com>
2022-10-14 09:32:00 +0300

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ApplicationRecord
+    validates :username, presence: true, 
+                        uniqueness: { case_sensitive: false }, 
+                        length: { minimum: 3, maximum: 25 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i        #all caps einai constant oxi variable
+    validates :email, presence: true, 
+                        uniqueness: { case_sensitive: false }, 
+                        length: { maximum: 105 },
+                        format: { with: VALID_EMAIL_REGEX }
+end 
 
-end
+
+
+
+#Some validations:
+# 1. validates :name, length: {minimum: 2, maximum: 10} validates the length 
+# 2. validates :points, numerically: true  --> numerically: {only_integer: true}    
+# 3. validates :name , :login , :email, presence:true  --> it uses the isblank? method
+# 4. validates :name , :login , :email, absence:true  --> the opposite it checks if the field is nil or blank 
+# 5 validates :email, uniqueness: true  --> validates that the attribute values is unique.It does not create uniqueness constrain in the database.
